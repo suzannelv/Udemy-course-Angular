8f60b9108c3ea15f3e4c7e2f096c29e104858ab8
suzannelv <lvmeijuan9011@gmail.com>
2023-01-05 23:34:52 +0100

    2 commit

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
 alowNewServer = false;
+serverCreationStatus = 'No server was created!';
+serverName="";
 constructor(){
   setTimeout(() => {
     this.alowNewServer=true;
   }, 2000);
 }
+ngOnInit(): void {
+ 
+}
+ onCreateServer() {
+    this.serverCreationStatus = 'Server was created!'
+  }
+  onUpdateServerName(event:Event) {
+    // 通过添加HTML输入元素告知TYPESCRIPT中的类型,显示转换,并使用开始和结束标记
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
